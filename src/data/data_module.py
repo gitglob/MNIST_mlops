94d28d1e2141e9b98057cea4d0644e8f6ec42ff2
gitglob <pgrigor22@gmail.com>
2023-02-20 17:50:31 +0100

    transferred code to pytorch lightning

---
+++ src/data/data_module.py
@@ -0,0 +1,119 @@
+import pytorch_lightning as pl
+from torch.utils.data import random_split, DataLoader, Dataset
+from numpy import load
+from torchvision import transforms
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class MnistDataset(Dataset):
+    """
+    MNIST fashion dataset based on numpy arrays.
+
+    Attributes
+    ----------
+    images: np.ndarray
+        the dataset images
+    transform: torchvision.transforms
+        the image transform
+    labels: np.ndarray
+        the dataset labels
+    ineference : bool
+        indicates if there is are labels for the current dataset.
+    """
+
+    def __init__(self, images, labels=None, transform=None, stage=None):
+        """Initializes images, labels and inference flag."""
+        self.images = images
+        self.transform = transform
+        self.stage = stage
+        if self.stage != "predict":
+            self.labels = labels
+
+    def __len__(self):
+        """Gets the size of the dataset."""
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        """Gets the image with the specified id."""
+        images = self.images[idx]
+        if self.transform:
+            images = self.transform(images).float()
+        if self.stage != "predict":
+            labels = self.labels[idx]
+            return images, labels
+        else:
+            return images
+
+
+class MnistDataModule(pl.LightningDataModule):
+    """
+    Loads the saved tensors in data/processed, normalizes them and returns the DataLoaders
+    through its methods.
+    """
+
+    def __init__(self, data_dir: str = "data/processed", batch_size: int = 32):
+        super().__init__()
+        log.info(f"Loading data from: {data_dir}")
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.transform = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize(mean=[0.485], std=[0.229])]
+        )
+
+    def prepare_data(self):
+        return
+
+    def setup(self, stage: str):
+        if stage == "fit":
+            train_images = load(self.data_dir + "/train_images.npy")
+            train_labels = load(self.data_dir + "/train_labels.npy")
+            dataset_full = MnistDataset(
+                train_images, train_labels, transform=self.transform
+            )
+            train_size = int(len(dataset_full) * 0.9)
+            val_size = len(dataset_full) - train_size
+            self.train_dataset, self.val_dataset = random_split(
+                dataset_full, [train_size, val_size]
+            )
+
+        if stage == "test":
+            test_images = load(self.data_dir + "/test_images.npy")
+            test_labels = load(self.data_dir + "/test_labels.npy")
+            self.test_dataset = MnistDataset(
+                test_images, test_labels, transform=self.transform
+            )
+
+        if stage == "predict":
+            self.predict_dataset = MnistDataset(
+                test_images, test_labels, transform=self.transform, stage=stage
+            )
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=16
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=16
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=16
+        )
+
+    def predict_dataloader(self):
+        return DataLoader(
+            self.predict_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=16,
+        )
+
+    def teardown(self, stage: str):
+        # Used to clean-up when the run is finished
+        return
--- src/models/model.py
+++ src/models/model.py
@@ -1,14 +1,14 @@
 from typing import Tuple
+from torch import Tensor
 
-import torch
-import torch.nn.functional as F
-from torch import nn
-import logging
+from pytorch_lightning import LightningModule
+from torch import nn, optim
 
-log = logging.getLogger(__name__)
+import wandb
+import numpy as np
 
 
-class MyModel(nn.Module):
+class MyModel(LightningModule):
     """
     A class for a NN to train and make predictions on MNIST data.
 
@@ -29,14 +29,14 @@ class MyModel(nn.Module):
         log.infos the person's name and age.
     """
 
-    def __init__(self, input_dim, latent_dim, output_dim):
+    def __init__(self, img_shape, latent_dim, output_dim):
         """
         Constructs all the layers of the NN.
 
         Parameters
         ----------
-        input_dim : int
-            size of input dimension
+        img_shape : int
+            shape of image (x, y, z)
         latent_dim : int
             size of latent dimension (latent features)
         output_dim : int
@@ -44,15 +44,26 @@ class MyModel(nn.Module):
         """
 
         super().__init__()
-        self.in_fc = nn.Linear(input_dim, 512)
-        self.fc2 = nn.Linear(512, 128)
-        self.fc3 = nn.Linear(128, latent_dim)
-        self.out_fc = nn.Linear(latent_dim, output_dim)
+        self.backbone = nn.Sequential(
+            nn.Conv2d(1, 64, 3),
+            nn.LeakyReLU(),
+            nn.Conv2d(64, 32, 3),
+            nn.LeakyReLU(),
+            nn.Conv2d(32, 16, 3),
+            nn.LeakyReLU(),
+            nn.Conv2d(16, 8, 3),
+            nn.LeakyReLU(),
+        )
 
-        self.dropout = nn.Dropout(0.5)
-        self.softmax = nn.Softmax()
+        self.latent_extractor = nn.Sequential(
+            nn.Flatten(), nn.Linear(8 * 20 * 20, latent_dim)
+        )
 
-    def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        self.classifier = nn.Sequential(nn.Dropout(), nn.Linear(latent_dim, output_dim))
+
+        self.criterium = nn.CrossEntropyLoss()
+
+    def forward(self, x: Tensor) -> Tuple[Tensor, Tensor]:
         """
         Does a forward pass on the NN
 
@@ -67,21 +78,16 @@ class MyModel(nn.Module):
             the 2d features before the output layer & the output of the NN
         """
         # self.pshape(x)
-        x = F.relu(self.in_fc(x))
-        x = self.dropout(x)
+        x = self.backbone(x)
         # self.pshape(x)
-        x = F.relu(self.fc2(x))
-        x = self.dropout(x)
+        latent_features = self.latent_extractor(x)
         # self.pshape(x)
-        features = F.relu(self.fc3(x))
-        x = self.dropout(features)
-        # self.pshape(x)
-        x = self.out_fc(x)
+        y = self.classifier(latent_features)
         # self.pshape(x)
 
-        return features, x
+        return y
 
-    def pshape(self, x: torch.Tensor) -> None:
+    def pshape(self, x: Tensor) -> None:
         """
         prints the shape of the input tensor
 
@@ -91,3 +97,45 @@ class MyModel(nn.Module):
             input tensor
         """
         print("Shape: ", x.shape)
+
+    def training_step(self, batch, batch_idx):
+        loss, acc = self._shared_eval_step(batch, batch_idx)
+
+        # log with wandb
+        self.log("train_loss", loss)
+        self.log("train_acc", acc)
+
+        # hist = np.histogram(preds.detach().numpy())
+        # self.logger.experiment.log({'train_logits': wandb.Histogram(np_histogram=hist)})
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss, acc = self._shared_eval_step(batch, batch_idx)
+        metrics = {"val_acc": acc, "val_loss": loss}
+        self.log_dict(metrics)
+        return metrics
+
+    def test_step(self, batch, batch_idx):
+        loss, acc = self._shared_eval_step(batch, batch_idx)
+        metrics = {"test_acc": acc, "test_loss": loss}
+        self.log_dict(metrics)
+        return metrics
+
+    def _shared_eval_step(self, batch, batch_idx):
+        # extract x,y and forward pass
+        x, y = batch
+        preds = self(x)
+        # loss and accuracy
+        loss = self.criterium(preds, y)
+        acc = (y == preds.argmax(dim=-1)).float().mean()
+        return loss, acc
+
+    def predict_step(self, batch, batch_idx, dataloader_idx=0):
+        x, y = batch
+        preds = self(x)
+        return preds
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
